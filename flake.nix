32a3e80e226620fc62f8c8505817391c1d64a1c4
Daniele Pira <pira.daniele@gmail.com>
2023-09-19 10:45:19 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            elixir_1_11
+            erlang_24
+            rebar3
+          ];
+        };
+      }
+    );
+}
+
